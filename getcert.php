125389a9fcab0365f892daf1db49994a328b86c5
Ioannis Raptis <raptis.y@gmail.com>
2016-02-04 13:28:46 +0200

    more options

--- getcert.php
+++ getcert.php
@@ -1,13 +1,15 @@
 <?php
 //header("Access-Control-Allow-Origin: http://ckan.linkedeconomy.org");  //uncomment and change the header if it is needed for cross-origin issues
 
+
 //options start
-$debug='off';     // on/off, if it works, change this value to 'off', so that you don't recieve debug messages
-$method=-1;     // -1/0/1/2/3,  choose a method to read the js file containing the certificate info, 
-               //            VALUES:  -1:test all methods,0:cURL,1:fopen,2:file_get_contents,3:http_get()
+$debug='off';     // on/off, if it works, change this value to 'off', so that it will not display debug messages anymore
+$method=-1;       // -1/0/1/2/3,  choose a method to read the js file containing the certificate info, VALUES:  -1:try all methods,0:cURL,1:fopen,2:file_get_contents,3:http_get()
+$projection=1;    // 0/1,   choose a method to show the badge   0:our implementation, 1:ODI implementation
+$size=100;         // 0 to 100,   set the size of the badge and letters
 //options end
 
-if($debug=='on') echo '<p style="color:red;">-php file loaded succesfully</p>';
+if($debug=='on') echo '<p style="color:green;">-php file loaded succesfully</p>';
 
 $getype=0;
 $ferror=0;
@@ -15,6 +17,7 @@ $raw='Bronze Open Data Certificate';
 $pilot='Silver Open Data Certificate';
 $standard='Gold Open Data Certificate';
 $expert='Platinum Open Data Certificate';
+$autom='automatically awarded';
 $success="false";
 ini_set("allow_url_fopen", true);
 
@@ -58,13 +61,36 @@ if($tot==3) {
   }else return "false";
 }
 
-function check_state($state) {
-	$r_txt = '<p style="color:red;">-Debug returned state value ';
-	if($state==0) $r_txt = $r_txt . '0: normal operation';
-	   else if($state==1) $r_txt = $r_txt . '1: not able to load certificate information';
-	       else if($state==2) $r_txt = $r_txt . '1: not able to load the csv files';
-		   
-     return $r_txt;
+function check_state($state) {	
+	if($state==0) $r_txt = '<p style="color:green; text-align:center;">-Debug returned state value 0:_normal_operation_</p>';
+	   else if($state==1) $r_txt = '<p style="color:red; text-align:center;">-Debug returned state value 1:_not_able_to_load_certificate_information_</p>';
+	       else if($state==2) $r_txt = '<p style="color:red; text-align:center;">-Debug returned state value 2:_not_able_to_load_the_csv_files_</p>';		   
+        return $r_txt;
+}
+
+function projection1($url, $nm) {     
+    
+    global $raw, $expert, $standard, $pilot, $size;
+    $bgurlf = file_get_contents($url);
+    
+    echo '<div class=\'open-data-certificate\'> <style>@import url(https://certificates.theodi.org/assets/badge.css);
+</style><a href="https://certificates.theodi.org/en/datasets' . $nm . '/certificate">  <img alt="Badge" src="https://certificates.theodi.org/en/datasets' . $nm . '/certificate/badge.png" style="width:' . ($size-50) . '%;height:' . ($size-50) . '%;" /> </a><ul class=\'open-data-certificate-details\'> <li><span> </span> </li> <li> <span><font style="font-size:' . $size . '%;text-align:left;">';
+    $pos = strpos($bgurlf, $raw);
+                     if($pos===false) {
+                        $pos = strpos($bgurlf, $pilot);
+                        if($pos===false)  {
+                               $pos = strpos($bgurlf, $standard);
+                               if($pos===false)  {
+                                    $pos = strpos($bgurlf, $expert);
+                                    if($pos===false)  {
+                                       echo 'No Level Certificate'; 
+                                       }else echo $expert;
+                                    }else echo $standard;
+                                }else echo $pilot;
+                        }else echo $raw;
+
+echo '</font></span> </li> <li><font style="font-size:' . $size . '%;"> <span>Active - automatically awarded</span></font> </li> </ul></div><br><br><br>';
+
 }
 
 for($i=1;$i<200;$i++) { 
@@ -74,32 +100,36 @@ for($i=1;$i<200;$i++) {
        $entries=0;
        $tmp = file_get_contents($_GET[$ar]);
        if ($tmp=='') { if($debug=='on') echo '<p style="color:red;">-check url path: ' . $_GET[$ar] . '</p>'; $ferror=2; break;}
-           else if($debug=='on') echo '<p style="color:red;">-Searching certificates in ' . $_GET[$ar];
+           else if($debug=='on') echo '<p style="color:grey;">-Searching certificates in ' . $_GET[$ar];
        $res = explode("\n", $tmp);
-       if($debug=='on') echo '(' . (count($res)-1) . '_entries)';   
+       if($debug=='on') echo '(' . (count($res)-1) . '_entries)</p>';   
           for($j=0;$j<count($res);$j++) {
               if($debug=='on') $entries++;
               $res2 = explode(",", $res[$j]);
               $res3 = checkline($res2);      
               if($res3!="false") {
-                   if($debug=='on') echo ' -connection found in line #' . $j . '</p>';                
+                   if($debug=='on') echo '<p style="color:green;"> -connection found in line #' . $j . '</p>';                
                    $jsurl= 'https://certificates.theodi.org/en/datasets' . $res3 . '/certificate/badge.js';  
-                   if($debug=='on') echo '<p style="color:red;">----trying_methods_to_read_url:_' . $jsurl . '<br>';                
+                   if($projection==1) {
+                                 projection1($jsurl, $res3);
+                                 return;
+                                 }
+                   if($debug=='on') echo '<p style="color:grey;">----trying_methods_to_read_url:_' . $jsurl . '<br>';                
                    if($method!=-1) $getype=$method;
                    if($getype==0) {
-                          if($debug=='on')   echo '<p style="color:red;"> -trying method 0,'; 
+                          if($debug=='on')   echo '<p style="color:grey;"> -trying method 0..</p>'; 
                           try{              
                                 $bgurl = fetchUrl($jsurl);
                                 }catch(Exception $e) {
                                         if ($bgurl=='') {
-										     if($debug=='on') echo ' failed to open file ' . '<br>'; 
+					     if($debug=='on') echo '<p style="color:red;"> failed to open file ' . '</p>'; 
                                              if($method==-1) $getype=1;
 									    	  }
                                          }
                     } 
 
                    if($getype==1) {
-                          if($debug=='on') echo '<p style="color:red;"> -trying method 1,'; 
+                          if($debug=='on') echo '<p style="color:grey;"> -trying method 1..'; 
                           if (function_exists('fopen') && function_exists('stream_get_contents') && function_exists('fclose')) {
                                  $stream=fopen($jsurl,'r');
                                  $bgurl = stream_get_contents($stream);
@@ -107,31 +137,31 @@ for($i=1;$i<200;$i++) {
 	                             }
 
                           if ($bgurl=='') {
-		                      if($debug=='on') echo ' failed to open file ' . '<br>'; 
+		                      if($debug=='on') echo '<p style="color:red;"> failed to open file ' . '</p>'; 
                               if($method==-1) $getype=2;
 		                      }
                     }
 
                    if($getype==2) {
-                          if($debug=='on') echo '<p style="color:red;"> -trying method 2,';
+                          if($debug=='on') echo '<p style="color:grey;"> -trying method 2..';
                           if (function_exists('file_get_contents')) $bgurl = file_get_contents($jsurl);
                           if ($bgurl=='') {
-	                      	 if($debug=='on') echo ' failed to open file ' . '<br>'; 
+	                      	 if($debug=='on') echo '<p style="color:red;"> failed to open file ' . '</p>'; 
                              if($method==-1) $getype=3;
 	                       	 }
                     }
 
                    if($getype==3) {
-                         if($debug=='on') echo '<p style="color:red;"> -trying method 3,';
+                         if($debug=='on') echo '<p style="color:grey;"> -trying method 3..';
                          if(function_exists('http_get')) $bgurl = http_get($jsurl, array("timeout"=>1), $info);
                          if($bgurl=='') {
-	                    	 if($debug=='on') echo ' failed to open file ' . '<br>'; 
+	                    	 if($debug=='on') echo '<p style="color:red;"> failed to open file ' . '</p>'; 
                              if($method==-1) $getype=0;
 		                     }
                     }
 
-                    if ($bgurl=='') {if($debug=='on') echo '<p style="color:red;">-dataset #' . $res3 . ' No method could read certificate info, Possible reasons: <br> i) It does not exist(check your csv file)<br> ii) Open Data Server is down<br> iii) A different method is needed<br> iv) Enable extension=php_http.dll or extension=php_curl.dll in your php.ini file </p>'; $ferror=1;}
-                    echo '<p style="text-align:center"><a href="https://certificates.theodi.org/en/datasets' . $res3 . '/certificate" target="_blank"><img src="' . 'https://certificates.theodi.org/en/datasets' . $res3 . '/certificate/badge.png"></a>'; 
+                    if ($bgurl=='') {if($debug=='on') echo '<p style="color:red;">-dataset #' . $res3 . ' No method could read certificate info, Possible reasons: <br> i) It does not exist(check your csv file)<br> ii) Open Data Server is down<br> iii) A different method is needed<br> iv) Enable extension=php_http.dll or extension=php_curl.dll in your php.ini file </p>'; $ferror=1;} else if($debug=='on') echo '<p style="color:green;">-Certificate info loaded</p>';
+                    echo '<p style="text-align:center;"><a href="https://certificates.theodi.org/en/datasets' . $res3 . '/certificate" target="_blank"><img style="width:' . ($size-30) . '%;height:' . ($size-30) . '%;" src="' . 'https://certificates.theodi.org/en/datasets' . $res3 . '/certificate/badge.png"></a>'; 
                     $res4=$res3;
                     $tmp2 = $bgurl;                  
                     $success="true";
@@ -145,7 +175,7 @@ if($success=="false" || $ferror!=0) {
 	     echo '<p></p>'; 
 		 if($debug=='on') echo '<p style="color:red;">Certificate info could not be retrieved</p>';
 		 } else {    
-                  echo '<br><a href="https://certificates.theodi.org/en/datasets' . $res4 . '/certificate" target="_blank"><table style="border-collapse: collapse;"><tr><p style="text-align:center">';    
+                  echo '<br><a href="https://certificates.theodi.org/en/datasets' . $res4 . '/certificate" target="_blank"><table style="border-collapse: collapse;"><tr><p style="text-align:center;font-size:' . $size . '%;">';    
                      $pos = strpos($tmp2, $raw);
                      if($pos===false) {
                         $pos = strpos($tmp2, $pilot);
@@ -162,10 +192,12 @@ if($success=="false" || $ferror!=0) {
 
                   $pos = strpos($tmp2, $autom);
                   if($pos===false) { 
-				      echo '</p></tr><tr><p style="text-align:center; font-size:80%;">self certified</p></tr></table>'; 
-					  } else echo '</p></tr><tr><p style="text-align:center; font-size:80%;">' . $autom . '</p></tr></table>';
+				      echo '</p></tr><tr><p style="text-align:center; font-size:' . ($size-20) . '%;">self certified</p></tr></table>'; 
+					  } else echo '</p></tr><tr><p style="text-align:center; font-size:' . ($size-20) . '%;">' . $autom . '</p></tr></table>';
                   echo '</a></p>';
 }
-if($debug=='on') echo '<p style="color:red;">-Debug is ON, if you don\'t want to see this text please change the value of the $debug variable to OFF.</p>';
+
 if($debug=='on') echo check_state($ferror);
+if($debug=='on') echo '<p style="color:grey;">-Debug is ON, if you don\'t want to see this text please change the value of the $debug variable to OFF.</p>';
+
 ?>
